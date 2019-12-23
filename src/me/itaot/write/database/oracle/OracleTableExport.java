140ce126048f2e97e51b15c78c68977e408f5c44
wutaotao <wuttc@dcits.com>
2019-12-23 21:56:31 +0800

    表字段错误修复

--- src/me/itaot/write/database/oracle/OracleTableExport.java
+++ src/me/itaot/write/database/oracle/OracleTableExport.java
@@ -14,12 +14,25 @@ import me.itaot.database.tools.DBTools;
 import me.itaot.write.word.WriteWordFile;
 
 public class OracleTableExport {
-	
+
 	private static final Logger log = Logger.getLogger(OracleTableExport.class);
 
-	private static final String SQL_TABLE_STRUCT = "SELECT a.column_name AS column_name,a.data_type AS data_type,a.data_length as data_length,a.nullable AS nullable,comments  FROM user_tab_columns a LEFT JOIN user_col_comments b ON (a.table_name = b.table_name AND a.column_name = b.column_name) WHERE a.table_name = ?  ORDER BY column_id";
+	private static final String SQL_TABLE_STRUCT = "SELECT A.COLUMN_NAME                                                     AS COLUMN_NAME,\r\n"
+			+ "       A.DATA_TYPE                                                       AS DATA_TYPE,\r\n"
+			+ "       CASE\r\n" + "           WHEN A.DATA_TYPE = 'NUMBER' THEN CASE\r\n"
+			+ "                                                WHEN A.DATA_SCALE = 0 THEN TO_CHAR(A.DATA_PRECISION)\r\n"
+			+ "                                                ELSE '(' || A.DATA_PRECISION || ',' || A.DATA_SCALE || ')' END\r\n"
+			+ "           WHEN A.DATA_TYPE = 'VARCHAR2' THEN TO_CHAR(A.CHAR_LENGTH) END AS DATA_LENGTH,\r\n"
+			+ "       A.NULLABLE                                                        AS NULLABLE,\r\n"
+			+ "       COMMENTS  FROM user_tab_columns a LEFT JOIN user_col_comments b ON (a.table_name = b.table_name AND a.column_name = b.column_name) WHERE a.table_name = ?  ORDER BY column_id";
 
-	private static final String SQL_QUERY_ALL_TABLES = "select a.table_name as table_name,b.comments as comments from user_tables a,user_tab_comments b where a.table_name = b.table_name and  a.table_name not like '%$%' and a.table_name not like '%BAK_BWZL_ZBM%' order by table_name";
+	private static final String SQL_QUERY_ALL_TABLES = "select a.table_name as table_name,b.comments as comments from user_tables a,user_tab_comments b "
+			+ "											where a.table_name = b.table_name and  a.table_name not like '%$%' "
+			+ "											  and a.table_name not like '%BAK_BWZL_ZBM%' "
+			+ "											  and a.table_name not like '%MIVS%'"
+			+ "											  and a.table_name not like '%BATCH_%'"
+			+ "											  and b.comments is not null"
+			+ "											  order by table_name";
 
 	public static void main(String args[]) throws SQLException, IOException {
 		WriteWordFile wordFile = new WriteWordFile(ConfigTools.get("file"));
@@ -34,19 +47,19 @@ public class OracleTableExport {
 		while (rs.next()) {
 			String tableName = rs.getString("table_name");
 			String tableComments = rs.getString("comments");
-			log.info("表名：" + tableName);
+			log.info("表名：" + tableName + "\t " + tableComments);
 			long startTime = System.currentTimeMillis();
-			
+
 			Connection conn2 = DBTools.getConnFromPool();
 			PreparedStatement pmtm = conn2.prepareStatement(SQL_TABLE_STRUCT);
 			pmtm.setString(1, tableName);
 			ResultSet tableStructRs = pmtm.executeQuery();
-			
+
 			wordFile.writeTableStruct(tableName, tableComments, tableStructRs);
 			tableStructRs.close();
 			pmtm.close();
 			conn2.close();
-			
+
 			long endTime = System.currentTimeMillis();
 			log.info("写入完成，耗时" + (endTime - startTime) + "ms");
 		}
